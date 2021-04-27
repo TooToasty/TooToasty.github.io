f9df593de5d82d078575552b8137bbc48f659818
Noah Derby <49260137+TooToasty@users.noreply.github.com>
2021-04-26 22:51:09 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,4 +1,5 @@
-function sleep(ms) {
+/*
+  function sleep(ms) {
   return new Promise(
     resolve => setTimeout(resolve, ms)
   );
@@ -8,3 +9,4 @@ function sleep(ms) {
 var element = document.getElementById("test");
 sleep(1000);
 element.innerHTML= "woah did it change";
+*/ 
